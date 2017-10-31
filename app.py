2df09330a9dcc1e82aeff66a8969a13e23d16f18
Qiu <qiu.gits@gmail.com>
2017-11-01 06:26:18 +0900

    add sbi-history-analyzer

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+from mymetrics.sbi_trade_history import rearrange_trade_data
+import pandas as pd
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -11,6 +14,20 @@ def about():
         ('Github', 'https://github.com/qiugits'),
     ]
     return render_template('about.html', accounts=accounts)
+
+
+@app.route('/sbi', methods=['GET', 'POST'])
+def sbi():
+    """ analyze sbi history """
+    if request.method == 'GET':
+        return render_template('sbi.html')
+    else:  # == 'POST'
+        ifile = request.files['file']
+        df = pd.read_csv(ifile, encoding='Shift-JIS', skiprows=5)
+        res = rearrange_trade_data(df)
+        ofile = res.to_csv().encode('utf-8')
+        return send_file(BytesIO(ofile), attachment_filename='outfile.csv',
+                         as_attachment=True)
 
 
 if __name__ == '__main__':
---
+++ mymetrics/sbi_trade_history.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+
+
+def rearrange_trade_data(df):
+    # df[[1, 4,5,6, 14, 17]]
+    _df = df[['約定日時', '取引', '約定価格', '約定数量', '新規建単価', '決済損益']]
+
+    def gen_row(): return ['', '', 0, 0, '', 0, 0, 0]
+    _columns = ['order time', 'settle time', 'order price', 'settle price',
+                'position', 'tickets', 'result', 'profit/loss']
+    en = {'新規買': 'K', '新規売': 'U'}
+    tmp = []
+    for i in reversed(_df.values):
+        if '決済' in i[1]:
+            t = gen_row()
+            t[1], t[2], t[3], t[5], t[7] =\
+                i[0], int(i[4]), i[2], i[3], int(i[5])
+            if '売' in i[1]:
+                t[4] = 'K'
+                t[6] = t[3] - t[2]
+            elif '買' in i[1]:
+                t[4] = 'U'
+                t[6] = t[2] - t[3]
+            tmp.append(t)
+    for i in reversed(_df.values):
+        if '新規' in i[1]:
+            for t in reversed(tmp):
+                if not t[0] and t[2] == i[2] and en[i[1]] == t[4]:
+                    t[0] = i[0]
+                    break
+            else:
+                print(i)
+    # print(*tmp[:5], sep='\n')
+    return pd.DataFrame(tmp, columns=_columns)
+
+
+def evaluate_risk_reward(chart_result):
+    cr = chart_result[['result']].values
+    _win = list(filter(lambda x: x > 0, cr))
+    _lose = list(filter(lambda x: x < 0, cr))
+    ave_win = np.average(_win)
+    ave_lose = np.average(_lose)
+    num_win = len(_win)
+    num_lose = len(_lose)
+    win_rate = num_win / (num_win + num_lose)
+    risk_reward = ave_win / abs(ave_lose)
+    return (ave_win, ave_lose, num_win, num_lose, win_rate, risk_reward)
+
+
+# if __name__ == '__main__':
+#     _fname = sorted(glob.glob('/Users/qiushi/Downloads/ExecutionRefer[0-9]*.csv'))[-1]
+#     print(f'reading filename {_fname}')
+#     df = pd.read_csv(_fname, encoding='Shift-JIS', skiprows=5)
+# 
+#     res = rearrange_trade_data(df)
+#     res.to_csv('/Users/qiushi/Desktop/rearranged.csv')
+# 
+#     cs_res = np.cumsum(res[['result']].values.T)
+#     cs_pro = np.cumsum(res[['profit/loss']].values.T) / 100
+#     print(max(cs_res) - min(cs_res))
+#     plt.plot(cs_res)
+#     plt.plot(cs_pro)
+#     plt.savefig('/Users/qiushi/Desktop/fig.png')
+#     plt.show()
+
