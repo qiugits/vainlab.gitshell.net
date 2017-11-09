9525844a265cfff60264bfa3012c6c2daf832695
Qiu <qiu.gits@gmail.com>
2017-11-10 03:42:24 +0900

    add plot graph function

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, send_file
-from mymetrics.sbi_trade_history import (
-    rearrange_trade_data, evaluate_risk_reward
-)
+from mymetrics.sbi_trade_history import SbiAnalyzer
 from mymetrics.analytics import track_event
 from mymetrics.environment import if_is_development
 import pandas as pd
 from io import BytesIO
+# デバッグをしたい場合
+# import pdb;  pdb.set_trace()
 
 
 app = Flask(__name__)
@@ -16,55 +16,49 @@ app = Flask(__name__)
 def about():
     """ about """
     track_event('about', 'get')
-    meta = {
-        'title': 'About',
-    }
 
     accounts = [
         ('Qiita', 'https://qiita.com/qiugits'),
         ('Github', 'https://github.com/qiugits'),
     ]
-    return render_template('about.html', meta=meta, accounts=accounts)
+    return render_template('about.html', accounts=accounts)
 
 
 @app.route('/sbi')
 def sbi():
     """ analyze sbi history """
     track_event('sbi', 'get')
-    meta = {
-        'title': 'SBI',
-    }
 
     buttons = [
-        # ('plt', 'グラフ', 'グラフをダウンロード'),
+        ('plt', 'グラフ', 'グラフを描画'),
         ('rr', 'RR分析', 'リスク・リワード分析の結果をダウンロード'),
         ('rearrange', 'データ整形', '整形されたデータをダウンロード'),
     ]
-    return render_template('sbi.html', meta=meta, buttons=buttons)
+    return render_template('sbi.html', buttons=buttons)
 
 
 @app.route('/sbi_button', methods=['POST'])
 def sbi_button():
     """ calculate sbi risk riward """
-    # import pdb;  pdb.set_trace()
 
     ifile = request.files['file']
-    df = pd.read_csv(ifile, encoding='Shift-JIS', skiprows=5)
-    res = rearrange_trade_data(df)
+    sbi = SbiAnalyzer(ifile)
 
     if request.form['action'] == 'plt':
         track_event('sbi', 'plt')
-        return 'plot graph(in test stage)'
+        bk_script, bk_div, bk_v = sbi.bokeh_plot_history()
+        return render_template('sbi-history.html',
+                               bk_script=bk_script, bk_div=bk_div, bk_v=bk_v)
     elif request.form['action'] == 'rr':
         track_event('sbi', 'rr')
-        rr = evaluate_risk_reward(res)
+        rr = sbi.evaluate_risk_reward()
         ofile = pd.DataFrame(rr).to_csv().encode('utf-8')
         return send_file(BytesIO(ofile),
                          attachment_filename='RiskReward.csv',
                          as_attachment=True)
     elif request.form['action'] == 'rearrange':
         track_event('sbi', 'rearrange')
-        ofile = res.to_csv().encode('utf-8')
+        ofile = sbi.df.to_csv().encode('utf-8')
         return send_file(BytesIO(ofile), attachment_filename='SBIhistory.csv',
                          as_attachment=True)
 
--- mymetrics/sbi_trade_history.py
+++ mymetrics/sbi_trade_history.py
@@ -2,67 +2,101 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh import __version__ as _bokeh_version
 
 
-def rearrange_trade_data(df):
-    # df[[1, 4,5,6, 14, 17]]
-    _df = df[['約定日時', '取引', '約定価格', '約定数量', '新規建単価', '決済損益']]
+class SbiAnalyzer:
+    """ SBI History Analyzer """
+    def __init__(self, _fname):
+        self.df = self._rearrange_trade_data(_fname)
 
-    def gen_row(): return ['', '', 0, 0, '', 0, 0, 0]
-    _columns = ['order time', 'settle time', 'order price', 'settle price',
-                'position', 'tickets', 'result', 'profit/loss']
-    en = {'新規買': 'K', '新規売': 'U'}
-    tmp = []
-    for i in reversed(_df.values):
-        if '決済' in i[1]:
-            t = gen_row()
-            t[1], t[2], t[3], t[5], t[7] =\
-                i[0], int(i[4]), i[2], i[3], int(i[5])
-            if '売' in i[1]:
-                t[4] = 'K'
-                t[6] = t[3] - t[2]
-            elif '買' in i[1]:
-                t[4] = 'U'
-                t[6] = t[2] - t[3]
-            tmp.append(t)
-    for i in reversed(_df.values):
-        if '新規' in i[1]:
-            for t in reversed(tmp):
-                if not t[0] and t[2] == i[2] and en[i[1]] == t[4]:
-                    t[0] = i[0]
-                    break
-            else:
-                print(i)
-    # print(*tmp[:5], sep='\n')
-    return pd.DataFrame(tmp, columns=_columns)
+    def _rearrange_trade_data(self, _fname):
+        """
+        parameter: fileIO (or filename. That doesn't matter to np)
+        returns:   df
+        """
 
+        _dtype = {
+            '約定価格': 'int32',
+            '約定数量': 'int16',
+            '手数料': 'int16',
+            '消費税': 'int16',
+            '約定金額': 'int32',
+            '受渡金額': 'int32',
+        }
+        df = pd.read_csv(_fname, encoding='Shift-JIS', skiprows=5,
+                         dtype=_dtype)
 
-def evaluate_risk_reward(chart_result):
-    cr = chart_result[['result']].values
-    _win = list(filter(lambda x: x > 0, cr))
-    _lose = list(filter(lambda x: x < 0, cr))
-    ave_win = np.average(_win)
-    ave_lose = np.average(_lose)
-    num_win = len(_win)
-    num_lose = len(_lose)
-    win_rate = num_win / (num_win + num_lose)
-    risk_reward = ave_win / abs(ave_lose)
-    rr = [
-        ('ave_win', ave_win),
-        ('ave_lose', ave_lose),
-        ('num_win', num_win),
-        ('num_lose', num_lose),
-        ('win_rate', win_rate),
-        ('risk_reward', risk_reward)
-    ]
-    return rr
+        # df[[1, 4,5,6, 14, 17]]
+        _df = df[['約定日時', '取引', '約定価格', '約定数量', '新規建単価', '決済損益']]
 
+        def gen_row(): return ['', '', 0, 0, '', 0, 0, 0]
+        _columns = ['order time', 'settle time', 'order price', 'settle price',
+                    'position', 'tickets', 'result', 'profit/loss']
+        en = {'新規買': 'K', '新規売': 'U'}
+        tmp = []
+        for i in reversed(_df.values):
+            if '決済' in i[1]:
+                t = gen_row()
+                t[1], t[2], t[3], t[5], t[7] =\
+                    i[0], int(i[4]), i[2], i[3], int(i[5])
+                if '売' in i[1]:
+                    t[4] = 'K'
+                    t[6] = t[3] - t[2]
+                elif '買' in i[1]:
+                    t[4] = 'U'
+                    t[6] = t[2] - t[3]
+                tmp.append(t)
+        for i in reversed(_df.values):
+            if '新規' in i[1]:
+                for t in reversed(tmp):
+                    if not t[0] and t[2] == i[2] and en[i[1]] == t[4]:
+                        t[0] = i[0]
+                        break
+                else:
+                    print(i)
+        # print(*tmp[:5], sep='\n')
+        return pd.DataFrame(tmp, columns=_columns)
 
-def plot_profit_history(df):
-    res = rearrange_trade_data(df)
-    cs_res = np.cumsum(res[['result']].values.T)
-    cs_pro = np.cumsum(res[['profit/loss']].values.T) / 100
-    print(max(cs_res) - min(cs_res))
-    plt.plot(cs_res)
-    plt.plot(cs_pro)
-    return plt
+    def evaluate_risk_reward(self):
+        cr = self.df[['result']].values
+        _win = list(filter(lambda x: x > 0, cr))
+        _lose = list(filter(lambda x: x < 0, cr))
+        ave_win = np.average(_win)
+        ave_lose = np.average(_lose)
+        num_win = len(_win)
+        num_lose = len(_lose)
+        win_rate = num_win / (num_win + num_lose)
+        risk_reward = ave_win / abs(ave_lose)
+        rr = [
+            ('ave_win', ave_win),
+            ('ave_lose', ave_lose),
+            ('num_win', num_win),
+            ('num_lose', num_lose),
+            ('win_rate', win_rate),
+            ('risk_reward', risk_reward)
+        ]
+        return rr
+
+    def plot_profit_history(self):
+        res = self.df
+        cs_res = np.cumsum(res[['result']].values.T)
+        cs_pro = np.cumsum(res[['profit/loss']].values.T) / 100
+        # print(max(cs_res) - min(cs_res))
+        plt.plot(cs_res)
+        plt.plot(cs_pro)
+        return plt
+
+    def bokeh_plot_history(self):
+        res = self.df
+        cs_res = np.cumsum(res[['result']].values.T)
+        cs_pro = np.cumsum(res[['profit/loss']].values.T) / 100
+        # print(max(cs_res) - min(cs_res))
+        x = np.arange(len(cs_pro))
+        p = figure()
+        p.line(x, cs_res, line_dash='dotted', line_color='orange', line_width=1.5)
+        p.line(x, cs_pro, line_width=2)
+        script, div = components(p)
+        return script, div, _bokeh_version
